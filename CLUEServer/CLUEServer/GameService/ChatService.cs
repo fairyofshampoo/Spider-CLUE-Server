b28a0c091787a7dc0ad6aa3fed29bcf0617207e1
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2023-12-13 14:28:00 -0600

    inicio de documentación y definiendo contrato del chat

---
+++ CLUEServer/CLUEServer/GameService/ChatService.cs
@@ -0,0 +1,28 @@
+﻿using GameService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService.Services
+{
+    public partial class ChatService : IChatManager
+    {
+        private static readonly Dictionary<string, IChatManagerCallback> chat
+        public void ConnectToChat(string username, int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisconnectFromChat(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(int code, Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
