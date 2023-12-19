27942c1a1df087202dec64b30175d743ad99d080
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2023-12-19 16:23:38 -0600

    creando una clase de constantes para ints de retorno, implementación de chat service

---
+++ CLUEServer/CLUEServer/GameService/Utilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService.Utilities
+{
+    public class Constants
+    {
+        public const int ERROR_IN_OPERATION = -1;
+        public const int SUCCESS_IN_OPERATION = 1;
+    }
+}
