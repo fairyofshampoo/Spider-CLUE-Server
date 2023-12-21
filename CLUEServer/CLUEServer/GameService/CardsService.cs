842590c60bb5cf2852dca00512977e7dee64abc1
Star3oy <eduarcaco@hotmail.com>
2023-12-21 15:35:25 -0600

    Cree los contratos para las cartas y el juego

---
+++ CLUEServer/CLUEServer/GameService/CardsService.cs
@@ -0,0 +1,60 @@
+﻿using GameService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService.Services
+{
+    public partial class GameService : ICardManager
+    {
+        public void ChooseCards(List<Card> first, List<Card> second, List<Card> third)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCharacterCards()
+        {
+            Card card1 =  new Card { ID = "Character1", Name = "Doc Och", Type = "Character"};
+            Card card2 =  new Card { ID = "Character2", Name = "MysteRevo", Type = "Character" };
+            Card card3 =  new Card { ID = "Character3", Name = "RhinoSidro", Type = "Character" };
+            Card card4 =  new Card { ID = "Character4", Name = "Electro-Juan", Type = "Character" };
+            Card card5 =  new Card { ID = "Character5", Name = "Xand-Man", Type = "Character" };
+            Card card6 =  new Card { ID = "Character6", Name = "Vernom", Type = "Character" };
+        }
+
+        public void CreatePlaceCards()
+        {
+            Card card7 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card8 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card9 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card10 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card11 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card12 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card13 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card14 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+            Card card15 = new Card { ID = "Character1", Name = "Doc Och", Type = "Character" };
+        }
+
+        public void CreateMotiveCards()
+        {
+
+        }
+
+        public void DealCards(string gamer1, string gamer2, string gamer3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShuffleCards(List<Card> cards)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CLUEServer/CLUEServer/GameService/Contracts/ICardManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService.Contracts
+{
+    [ServiceContract]
+    internal interface ICardManager
+    {
+        [OperationContract]
+        void CreatCards();
+
+        [OperationContract]
+        void ChooseCards(List<Card> first, List<Card> second, List<Card> third);
+
+        [OperationContract]
+        void ShuffleCards(List<Card> cards);
+
+        [OperationContract]
+        void DealCards(string gamer1, string gamer2, string gamer3);
+    }
+
+    [DataContract]
+    public class Card
+    {
+        private string id;
+        private string type;
+        private string name;
+
+        [DataMember]
+        public string ID { get { return id; } set { id = value; } }
+
+        [DataMember]
+        public string Type { get { return type; } set { type = value; } }
+
+        [DataMember]
+        public string Name { get { return name; } set { name = value; } }
+    }
+}
