a96255011b5895c235e3d07d7e9f3ec210bf7c5b
Star3oy <eduarcaco@hotmail.com>
2023-12-21 18:24:37 -0600

    Repartición de cartas creada

--- CLUEServer/CLUEServer/GameService/GameService.cs
+++ CLUEServer/CLUEServer/GameService/GameService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameService
+namespace GameService.Services
 {
     public partial class GameService : IGameManager
     {
