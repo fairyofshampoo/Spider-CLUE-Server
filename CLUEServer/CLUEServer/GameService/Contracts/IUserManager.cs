53904c67065d44354dfc0a47d2835daa1e536433
Michelle Tamariz <michellemoreno1313@gmail.com>
2024-01-05 14:23:23 -0600

    top 3 global

--- CLUEServer/CLUEServer/GameService/Contracts/IUserManager.cs
+++ CLUEServer/CLUEServer/GameService/Contracts/IUserManager.cs
@@ -1,12 +1,6 @@
-﻿using DataBaseManager;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameService.Contracts
 {
---
+++ CLUEServer/CLUEServer/GameService/WinnersService.cs
@@ -0,0 +1,21 @@
+﻿using DataBaseManager;
+using GameService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService
+{
+    public partial class GameService : IWinnersManager
+    {
+        public List<Winner> GetTopGlobalWinners()
+        {
+            using (var dataBaseContext = new SpiderClueDbEntities())
+            {
+                
+            }
+        }
+    }
+}
