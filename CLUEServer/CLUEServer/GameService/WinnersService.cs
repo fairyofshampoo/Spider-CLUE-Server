60ef17984696825deb072d21ec29b0c05ffbee8b
Star3oy <eduarcaco@hotmail.com>
2024-01-05 15:28:28 -0600

    Top 3 global agregado

--- CLUEServer/CLUEServer/GameService/WinnersService.cs
+++ CLUEServer/CLUEServer/GameService/WinnersService.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameService
+namespace GameService.Services
 {
     public partial class GameService : IWinnersManager
     {
@@ -14,7 +14,17 @@ namespace GameService
         {
             using (var dataBaseContext = new SpiderClueDbEntities())
             {
-                
+                var topGlobal = dataBaseContext.gamers
+                    .OrderByDescending(top => top.gamesWon)
+                    .Take(3)
+                    .Select(top => new Winner
+                    {
+                        Gamertag = top.gamertag,
+                        GamesWon = top.gamesWon,
+                        Icon =  top.imageCode
+                    })
+                    .ToList();
+                return topGlobal;
             }
         }
     }
