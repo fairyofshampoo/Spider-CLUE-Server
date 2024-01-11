cac2193c62ba13ca888984a468c653839bb12224
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-01-11 13:37:48 -0600

    arreglando cosillas

--- CLUEServer/CLUEServer/GameService/Utilities/Constants.cs
+++ CLUEServer/CLUEServer/GameService/Utilities/Constants.cs
@@ -8,13 +8,12 @@ namespace GameService.Utilities
 {
     public static class Constants
     {
-        public const int ERROR_IN_OPERATION = -1;
-        public const int SUCCESS_IN_OPERATION = 1;
-        public const string DEFAULT_ICON = "Icon0.jpg";
-        public const string DEFAULT_GUEST_NAME = "Guest";
-        public const int DEFAULT_GAMES_WON = 0;
-        public const string DEFAULT_GUEST_LAST_NAME = "Player";
-        public const int LIMIT_OF_GAMERS_IN_MATCH = 3;
-        public const int SUCCESS_IN_FRIEND_OPERATION = 2;
+        public const int ErrorInOperation = -1;
+        public const int SuccessInOperation = 1;
+        public const string DefaultIcon = "Icon0.jpg";
+        public const string DefaultGuestName = "Guest";
+        public const int DefaultGamesWon = 0;
+        public const string DefaultLastName = "Player";
+        public const int LimitOfGamersInMatch = 3;
     }
 }
